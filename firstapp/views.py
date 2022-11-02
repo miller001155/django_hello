d3c9dcbcd4231c214931230ebc4e3a7adacacf50
miller001155 <miller00115@gmail.com>
2022-11-02 18:58:18 +0300

    forms

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,12 +3,23 @@ from django.http import HttpResponse as fun
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.template.response import TemplateResponse as render_2  # то же самое что и render
+from .forms import UserForm
 
 
 def index(request):
-    return render(request, 'firstapp/index.html')
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     age = request.POST.get('age')
+    #     output = '<h2>Пользователь</h2><h3>Имя - {0}, Возраст - {1}<h3>'.format(name, age)
+    #     return fun(output)
+    # else:
+    #     userform = UserForm()
+    #     return render(request, 'firstapp/index.html', {'form': userform})
+    # return render(request, 'firstapp/index_old.html')
+    userform = UserForm(field_order=['ling', 'num'])
+    return render(request, 'firstapp/index.html', {'form': userform})
     # data = {'age': 66}
-    # return render(request, 'firstapp/index.html', context=data)
+    # return render(request, 'firstapp/index_old.html', context=data)
     # # data = {'header': 'Передача параметров в шаблон Django',
     # #         'message': 'Загружен шаблон templates/firstapp/index_app.html'}
     # # return render(request, 'firstapp/index_app1.html', context=data)
@@ -17,7 +28,7 @@ def index(request):
     # user = {'name': 'Влад', 'age': '28'}
     # addr = ('Виноградная', 23, 45)
     # data = {'header': header, 'langs': langs, 'user': user, 'addr': addr}
-    # return render(request, 'index.html', context=data)
+    # return render(request, 'index_old.html', context=data)
 
 
 def about(request):
